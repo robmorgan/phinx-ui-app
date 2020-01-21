86eccdac1bc0405a77f2251cfbf705b4c7632a06
Rob Morgan <robbym@gmail.com>
2020-01-21 14:17:27 +0800

    add phinx config

---
+++ phinx.php
@@ -0,0 +1,61 @@
+<?php
+
+use DI\ContainerBuilder;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require_once './vendor/autoload.php';
+
+// Instantiate PHP-DI ContainerBuilder
+$containerBuilder = new ContainerBuilder();
+
+if (false) { // Should be set to true in production
+	$containerBuilder->enableCompilation(__DIR__ . '/var/cache');
+}
+
+// Set up settings
+$settings = require __DIR__ . '/app/settings.php';
+$settings($containerBuilder);
+
+// Set up dependencies
+$dependencies = require __DIR__ . '/app/dependencies.php';
+$dependencies($containerBuilder);
+
+// Build PHP-DI Container instance
+$container = $containerBuilder->build();
+
+// Instantiate the app
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$settings = $container->get('settings');
+$config = $settings['db'];
+
+//var_dump($config);exit;
+
+return [
+    'paths'                => [
+        'migrations' => 'db/migrations',
+        'seeds'      => 'db/seeds',
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_database'        => 'development',
+        'development'             => [
+            'name'       => $config['database'],
+            'connection' => $container->get(PDO::class),
+        ],
+        'production'              => [
+            'adapter'   => 'mysql',
+            'host'      => $config['host'],
+            'name'      => $config['database'],
+            'user'      => $config['username'],
+            'pass'      => $config['password'],
+            'port'      => $config['port'],
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+    ],
+];
